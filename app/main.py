f3cd299076bcb87b648215fc3682f7309bda465a
Hoon Ui Lee <lhnui01@gmail.com>
2026-01-29 16:44:21 +0900

    delete api key

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from app.clients.gemma_client import GemmaClient
 from app.services.step1_extract import MenuExtractor
 from app.services.step2_rank import MenuRanker
 
-#MY_KEY = "AIzaSyC7Zgp9nWiFTkweTy2gzNXx-8xbogIxYHQ"
 gemma = GemmaClient(api_key=os.getenv("GOOGLE_API_KEY"))
 extractor = MenuExtractor(gemma)
 ranker = MenuRanker(gemma, uncertainty_penalty=60)
